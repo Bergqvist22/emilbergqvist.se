9bb41a32a93ba8d3a26037903ff41b4a46fb97c2
bergqvist22 <emil.bergqvist7@hotmail.com>
2026-05-27 13:05:25 +0200

    fixed cv

--- frontend/src/pages/CVPage.tsx
+++ frontend/src/pages/CVPage.tsx
@@ -3,8 +3,8 @@ import Navbar from "@/components/Navbar";
 import "./CVPage.css";
 
 const cvPages = [
-    "/public/Emil Bergqvist - CV-1-1_page-0001.jpg",
-    "/public/Emil Bergqvist - CV-1-1_page-0002.jpg",
+    "/public/Emil Bergqvist - CV-2026_page-0001.jpg",
+    "/public/Emil Bergqvist - CV-2026_page-0002.jpg",
 ];
 
 const contactLinks = [
